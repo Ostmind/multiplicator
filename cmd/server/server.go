b5404ff877fbb83706a9b91bd71ce9945d9304ad
Sergey <serz.sim@gmail.com>
2025-09-06 16:17:19 +0300

    Fix run problems

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,34 +2,32 @@ package main
 
 import (
 	"context"
+	"flag"
 	"github.com/Ostmind/multiplicator/internal/servers/config"
 	"github.com/Ostmind/multiplicator/internal/servers/logger"
 	"github.com/Ostmind/multiplicator/internal/servers/server"
 	"log"
-	"os"
-	"strconv"
+	"time"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Укажите параметр rtp при запуске: go run main.go <rtp>")
-	}
+	rtpPtr := flag.Float64("rtp", 0, "rtp parameter (must be > 0 and ≤ 1.0)")
+	flag.Parse()
 
-	rtp, err := strconv.ParseFloat(os.Args[1], 64)
-	if err != nil || rtp <= 0 || rtp > 1.0 {
+	if *rtpPtr <= 0 || *rtpPtr > 1.0 {
 		log.Fatal("rtp должен быть > 0 и ≤ 1.0")
 	}
 
 	cfg := config.MustNew()
-
 	sloger := logger.SetupLogger(cfg.EnvType)
+	srv := server.New(sloger, *rtpPtr)
 
-	srv := server.New(sloger, rtp)
-
-	srv.Run(cfg.Host, cfg.Port)
+	go srv.Run(cfg.Host, cfg.Port)
 
 	ctx, cancel := context.WithTimeout(context.Background(), cfg.ServerShutdownTimeout)
 	defer cancel()
+
+	time.Sleep(cfg.ServerShutdownTimeout)
 
 	srv.Stop(ctx)
 }
